87288e6d1691fa9b4586eb191fecf5d03a3c990e
kenneth <kenneth@yomane.com>
2026-02-15 23:57:14 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: process.env.FRONTEND_URL,
+		credentials: true,
+	});
+
 	const port = process.env.PORT || 3000;
 
 	await app.listen(port, '0.0.0.0');
